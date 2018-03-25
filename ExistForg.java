213594a22d514876c0d5c0bf4cdd92f0c80918b8
aarondevlin <31266650+aarondevlin@users.noreply.github.com>
2018-03-24 17:56:59 -0700

    edit prints

--- ExistForg.java
+++ ExistForg.java
@@ -4,9 +4,9 @@ import java.util.Random;
 public class ExistForg {
 
 	public static void main(String[] args) {
-		
+
 		/**
-		 * A random number is generated that is used in the bigInteger creator. This produces a random number that is 128 bits 
+		 * A random number is generated that is used in the bigInteger creator. This produces a random number that is 128 bits
 		 * that is modded by 10000 to assess the last 4 digits
 		 */
 		Random num = new Random(System.currentTimeMillis());
@@ -14,23 +14,23 @@ public class ExistForg {
 				+ "891728377199295753981791399033460102259322494388756606728373121043154809790249663472677206622549"
 				+ "24720490903440140409487830138442554051215639407252719582615491056895127372123401970340184655821416714383833567438594837829393436"
 				+ "445708175846840391647287652219983832401360628720836954408208209");
-		
-		System.out.println("Question3A");
-		
+
+		System.out.println("1st Problem");
+
 		BigInteger sigma = new BigInteger(128, num);
 		System.out.println(makeLastFour(sigma, e));
-		
+
 		BigInteger eb = new BigInteger("99985828020191359900880286869683035709839584003728838462445577041064"
 				+ "9259059950052168890075728986418115945133344092917628"
 				+ "7686491104489407462355371113514648093");
-		
-		System.out.println("Question3B");
+
+		System.out.println("2nd Problem");
 		for (int i = 0; i < 6; i++) {
 			sigma = new BigInteger(128, num);
 			System.out.println(makeLastFour(sigma, eb));
 		}
 	}
-	
+
 	// sigma is created. This is verified to have 2017 at the end of the message
 	public static String makeLastFour(BigInteger sigma, BigInteger e) {
 		BigInteger large = new BigInteger("10000");
